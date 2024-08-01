14a43d8d44e63f06b3c4c7687066656d9e4181cb
rfone <rfone@LAPTOP-T3C8FR4R>
2024-07-31 19:27:58 -0700

    added another test server to test server to server connections

--- TestServerProxy/Program.cs
+++ TestServerProxy/Program.cs
@@ -19,9 +19,38 @@ internal class Program
         }
 #endif
     }
-
+#if SERVER
     private static void onInit()
     {
-        throw new NotImplementedException();
+        isInit = true;
+        
+
+        RNet.RegisterGetConnectionTypeEvent(DetermineConnectionType);
+        RNet.RegisterOnClientConnectEvent(clientConnectLogicAction: LogicOnClientConnect);
+        RNet.RegisterOnServerConnectEvent(serverConnectLogicAction: LogicOnServerConnect);
+
+        RNet.InitializeServer("127.0.0.1", 7778, 255, 2048);
+    }
+
+   
+
+    private static void LogicOnServerConnect(Connection connection)
+    {
+        Console.WriteLine("Formed a connection with server");
+    }
+
+   
+    private static void LogicOnClientConnect(Connection connection)
+    {
+        Console.WriteLine("Formed a connection with a client");
+    }
+
+    private static ConnectionType DetermineConnectionType(string ip, ushort port)
+    {
+        if(ip.Equals("127.0.0.1") && port == 7777)
+            return ConnectionType.Server;
+        return ConnectionType.Client;
     }
 }
+
+#endif
